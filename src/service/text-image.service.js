65a40a32a1879beb353d9f46e13a10a4a29a2d0d
Adrian Gonzalo <gonzahh@gmail.com>
2019-07-09 17:03:53 +0200

    added new default format for image content

--- src/service/text-image.service.js
+++ src/service/text-image.service.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-plusplus */
 const text2png = require('text2png');
-const { paddText } = require('../utils/text');
+const { paddText, strikeText } = require('../utils/text');
 
 const DEFAULT_OPTIONS = {
   backgroundColor: 'white',
   output: 'dataURL',
+  padding: 10,
 };
 
 const createImage = (textContent, options = {}) => {
@@ -12,18 +13,30 @@ const createImage = (textContent, options = {}) => {
   return text2png(textContent, imageOptions);
 };
 
-const formatImageContent = (warriors = []) => {
+const formatImageContent = (warriors = [], { format = 'list' } = {}) => {
   if (!warriors.length) {
     return '';
   }
 
-  const COLUMNS = 3;
-  const MAX_NAME_LENGTH = 30;
+  // this works well on console, but the text2png lib breaks the format on image :/
+  if (format === 'three-columns') {
+    const COLUMNS = 3;
+    const MAX_NAME_LENGTH = 30;
+    return warriors.reduce((final, current, index) => {
+      const field = paddText(current.name, MAX_NAME_LENGTH, { strike: !current.alive });
+      return index > 0 && index % COLUMNS === 0 ? `${final}\n${field}` : `${final}${field}`;
+    }, '');
+  }
 
-  return warriors.reduce((final, current, index) => {
-    const field = paddText(current.name, MAX_NAME_LENGTH, { strike: !current.alive });
-    return index > 0 && index % COLUMNS === 0 ? `${final}\n${field}` : `${final}${field}`;
-  }, '');
+  const alive = warriors
+    .filter(warrior => warrior.alive)
+    .reduce((final, warrior) => `${final}\n${warrior.name}`, 'Alive: \n');
+
+  const defeated = warriors
+    .filter(warrior => !warrior.alive)
+    .reduce((final, warrior) => `${final}\n${strikeText(warrior.name)}`, 'Defeated: \n');
+
+  return `${alive}\n\n${defeated}`;
 };
 
 module.exports = {
